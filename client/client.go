653029df4c6b661b23b03e450093db96676c6352
MiaoJingyi <miaojingyi86@gmail.com>
2023-10-26 15:17:00 +0800

    .

--- client/client.go
+++ client/client.go
@@ -1,6 +1,12 @@
 package client
 
-import "math"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"math"
+	"time"
+)
 
 var SN28 = []int32{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27}
 
@@ -75,4 +81,58 @@ func ofGold(fGold float64) int32 {
 	}
 
 	return iGold
+}
+
+func sleepTo(s0 float64) {
+	d0 := time.Now().Sub(time.Now().Truncate(time.Minute))
+	if s0-d0.Seconds() < 0 {
+		panic(fmt.Sprintf("目标第%.2f秒小于当前第%.2f秒", s0, d0.Seconds()))
+	}
+
+	log.Printf("等待%.2f秒后继续执行 ... \n", s0-d0.Seconds())
+	time.Sleep(time.Second * time.Duration(s0-d0.Seconds()))
+}
+
+func Run(portGold, portBetting string) error {
+	// 配置文件
+	if err := InitConfig(); err != nil {
+		return err
+	}
+
+	// 数据库连接对象
+	db, err := sql.Open("mysql", conf.Dsn)
+	if err != nil {
+		return err
+	}
+
+	// Ping
+	if err := db.Ping(); err != nil {
+		return err
+	}
+	log.Println("连接数据库成功 ...")
+
+	// 如果超过30秒，那么等待30秒后运行
+	if time.Now().Second() >= 30 {
+		log.Printf("当前时间为%q，等待30秒 ... \n", time.Now().Format("2006-01-02 15:04:05"))
+		time.Sleep(30 * time.Second)
+	}
+
+	sleepTo(30)
+	go run(db, portGold, portBetting)
+
+	t := time.NewTicker(time.Minute)
+	defer t.Stop()
+
+	log.Println("启动定时器完成 ...")
+	for {
+		select {
+		case <-t.C:
+			// 长时间运行时，可能会产生时间偏移，自动调整
+			d0 := time.Now().Sub(time.Now().Truncate(time.Minute))
+			t.Reset(time.Duration(90-d0.Seconds()) * time.Second)
+			log.Printf("【重置时钟】偏移量%.2f秒 ...\n", 30-d0.Seconds())
+
+			go run(db, portGold, portBetting)
+		}
+	}
 }
